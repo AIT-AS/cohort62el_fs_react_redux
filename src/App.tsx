324512a8283da18771d08104d75d294d6812d039
AIT-AS <a.schuetz79@gmail.com>
2026-01-20 15:52:19 +0100

    Lesson_17

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// Lessons
+import Lesson_17 from "lessons/Lesson_17/Lesson_17"
+
+function App() {
+  return (
+  <>
+  <Lesson_17 />
+  </>
+  )
+}
+export default App
---
+++ src/components/Button/types.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+export type ButtonTypes = "submit" | "reset" | "button" | undefined;
+
+export interface ButtonProps {
+  name: string;
+  type?: ButtonTypes;
+  onClick?: () => void;
+  isRed?: boolean;
+  isDisabled?: boolean;
+}
